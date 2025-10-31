6d1d65d91358c60e0d8576c4d280277f6e917975
Dika yonanda putra <yonandaputra05@gmail.com>
2025-10-31 14:37:28 +0700

    update UI

--- resources/views/components/layouts/admin-header.blade.php
+++ resources/views/components/layouts/admin-header.blade.php
@@ -2,21 +2,24 @@
     <!-- Primary Navigation Menu -->
     <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <div class="flex justify-between h-16 items-center">
-            <!-- Mobile menu button -->
-            <div class="flex items-center lg:hidden">
-                <button @click="sidebarOpen = !sidebarOpen"
-                    class="inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:bg-gray-100 focus:text-gray-500 transition duration-150 ease-in-out">
-                    <svg class="h-6 w-6" stroke="currentColor" fill="none" viewBox="0 0 24 24">
-                        <path :class="{ 'hidden': sidebarOpen, 'inline-flex': !sidebarOpen }" stroke-linecap="round"
-                            stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h16" />
-                        <path :class="{ 'hidden': !sidebarOpen, 'inline-flex': sidebarOpen }" stroke-linecap="round"
-                            stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12" />
-                    </svg>
-                </button>
+            <!-- Left side - Logo/Brand (empty for now) -->
+            <div class="flex items-center">
+                <!-- Mobile menu button -->
+                <div class="flex items-center lg:hidden">
+                    <button @click="sidebarOpen = !sidebarOpen"
+                        class="inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:bg-gray-100 focus:text-gray-500 transition duration-150 ease-in-out">
+                        <svg class="h-6 w-6" stroke="currentColor" fill="none" viewBox="0 0 24 24">
+                            <path :class="{ 'hidden': sidebarOpen, 'inline-flex': !sidebarOpen }" stroke-linecap="round"
+                                stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h16" />
+                            <path :class="{ 'hidden': !sidebarOpen, 'inline-flex': sidebarOpen }" stroke-linecap="round"
+                                stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12" />
+                        </svg>
+                    </button>
+                </div>
             </div>
 
             <!-- Settings Dropdown -->
-            <div class="hidden sm:flex sm:items-center sm:ms-6 ml-auto">
+            <div class="hidden sm:flex sm:items-center sm:ms-auto">
                 <x-dropdown align="right" width="48">
                     <x-slot name="trigger">
                         <button
