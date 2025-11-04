b2389e7e13dedfca7a86c581da1513466af1afca
Dika yonanda putra <yonandaputra05@gmail.com>
2025-11-04 12:12:14 +0700

    change tinymce to textarea

--- resources/views/admin/article/create.blade.php
+++ resources/views/admin/article/create.blade.php
@@ -76,9 +76,8 @@
             <div>
                 <label for="body-editor" class="block mb-2 text-sm font-medium text-gray-900">Article
                     Body</label>
-                <x-forms.tinymce-editor>
-                    {{ old('body') }}
-                </x-forms.tinymce-editor>
+                <textarea name="body" id="body-editor" rows="10"
+                    class="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500">{{ old('body') }}</textarea>
                 @error('body')
                     <p class="mt-2 text-sm text-red-600">{{ $message }}</p>
                 @enderror
