cc535c6256d3f352736db213d7f602f9798be695
Dika yonanda putra <yonandaputra05@gmail.com>
2025-11-10 10:38:36 +0700

    delete partner CRUD UI

--- app/Http/Controllers/Admin/DashboardController.php
+++ app/Http/Controllers/Admin/DashboardController.php
@@ -14,6 +14,6 @@ class DashboardController extends Controller
         $totalArticles = Article::count();
         $totalServices = Service::count();
 
-        return view('admin.index', compact('totalArticles', 'totalServices', 'totalPartners'));
+        return view('admin.index', compact('totalArticles', 'totalServices'));
     }
 }
